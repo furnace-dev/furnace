84ec8af7a38aa17c44ff723e29b5348cca011554
yl <2201441955@qq.com>
2025-02-07 21:32:13 +0800

    fix: update

--- init.sh
+++ init.sh
@@ -3,10 +3,10 @@
 # Detect operating system
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
-    export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$(pwd)/.magic/envs/default/lib:$(pwd)"
+    export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$(pwd)/.magic/envs/default/lib:$(pwd)/bin"
 else
     # Linux
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/lib/x86_64-linux-gnu:$(pwd)/.magic/envs/default/lib:$(pwd)"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/lib/x86_64-linux-gnu:$(pwd)/.magic/envs/default/lib:$(pwd)/bin"
 fi
 
 # export LD_PRELOAD=libsonic.so:libfurnace_connect.so
